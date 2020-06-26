9225e26d6979948c635a5e35cd56a5b82da1750a
Mohammed Silmi <msilmi2@horizon.csueastbay.edu>
2020-06-26 14:31:15 -0700

    Add files via upload

---
+++ proxy_testing.py
@@ -0,0 +1,76 @@
+from lxml.html import fromstring
+import requests
+from itertools import cycle
+import traceback
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
+
+
+#If you are copy pasting proxy ips, put in the list below
+#proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
+proxies = get_proxies()
+proxy_pool = cycle(proxies)
+
+url = 'https://httpbin.org/ip'
+for i in range(1,11):
+    #Get a proxy from the pool
+    proxy = next(proxy_pool)
+    print("Request #%d"%i)
+    try:
+        response = requests.get(url,proxies={"http": proxy, "https": proxy})
+        print(response.json())
+    except:
+        #Most free proxies will often get connection errors. You will have retry the entire request using another proxy to work.
+        #We will just skip retries as its beyond the scope of this tutorial and we are only downloading a single url
+        print("Skipping. Connnection error")
+'''
+
+def my_proxy(PROXY_HOST,PROXY_PORT):
+        fp = webdriver.FirefoxProfile()
+        # Direct = 0, Manual = 1, PAC = 2, AUTODETECT = 4, SYSTEM = 5
+        print (PROXY_PORT)
+        print (PROXY_HOST)
+        fp.set_preference("network.proxy.type", 1)
+        fp.set_preference("network.proxy.http",PROXY_HOST)
+        fp.set_preference("network.proxy.http_port",int(PROXY_PORT))
+        fp.set_preference("general.useragent.override","whater_useragent")
+        fp.update_preferences()
+        return webdriver.Firefox(firefox_profile=fp)
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            #Grabbing IP and corresponding PORT
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
+
+
+proxies=get_proxies()
+print(proxies)
+
+
+
+for proxy in proxies:
+    
+    try:
+        PROXY_HOST,PROXY_PORT = proxy.split(':')
+        print(PROXY_HOST)
+        print(PROXY_PORT)
+        my_proxy(PROXY_HOST,PROXY_PORT)
+
+    except:
+        pass
